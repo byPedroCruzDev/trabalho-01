a860364b96516426afe3ec8975eb794a4f681e87
Pedro Cruz <98105642+byPedroCruzDev@users.noreply.github.com>
2023-08-15 22:16:55 -0400

    Update main.c
    add discription

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    printf("  ");
+    char vetor[6][20];
+
+    strcpy(vetor[0], "Pao com gergilin");
+    strcpy(vetor[1], "Alface americana");
+    strcpy(vetor[2], "Tomate");
+    strcpy(vetor[2], "Queijo Chedder");
+    strcpy(vetor[2], "Humburguer");
+    strcpy(vetor[2], "Nuggets");
+
 }
